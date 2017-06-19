ae79c13d000a8e71b8dc45d6be26774b44fa336a
alvin <alvin.odins@gmail.com>
2017-06-19 13:13:25 -0700

    create folder webdev component

---
+++ app/services/webdevelopment/webdevelopment.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'webdevelopment.component.html'
+})
+
+export class WebdevelopmentComponent implements OnInit {
+
+  ngOnInit(){}
+}
