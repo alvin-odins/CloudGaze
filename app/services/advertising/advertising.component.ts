c0141f440515d0bc9bce74620a32a2b8c9333a91
alvin <alvin.odins@gmail.com>
2017-06-19 13:12:10 -0700

    add folder for advertising component

---
+++ app/services/advertising/advertising.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'advertising.component.html'
+})
+
+export class AdvertisingComponent implements OnInit {
+
+  ngOnInit(){}
+}
