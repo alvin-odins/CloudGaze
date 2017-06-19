51b9d4bada7da74275310cc4288373517697e375
alvin <alvin.odins@gmail.com>
2017-06-19 13:07:05 -0700

    create navbar component

---
+++ app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'navbar-app',
+    templateUrl: './navbar.component.html'
+})
+
+export class NavbarComponent implements OnInit{
+  ngOnInit(){}
+}
