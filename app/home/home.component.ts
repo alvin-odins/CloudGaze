e32b33926ba9f3b3fe90236e47a1ad9c5fc71995
alvin <alvin.odins@gmail.com>
2017-06-19 13:07:44 -0700

    create home component

---
+++ app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector:'home-app',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.css']
+})
+
+export class HomeComponent implements OnInit{
+
+  ngOnInit(){}
+}
