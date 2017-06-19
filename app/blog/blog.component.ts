737f2a7d2599f073c94d6d8ab86018af0dcc3167
alvin <alvin.odins@gmail.com>
2017-06-19 13:09:03 -0700

    add blog component dir

---
+++ app/blog/blog.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'blog.component.html',
+  styleUrls: ['blog.css']
+})
+
+export class BlogComponent implements OnInit{
+
+  ngOnInit(){}
+}
