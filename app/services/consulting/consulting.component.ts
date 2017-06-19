a0406cda94129a87bb3c8e83f4de71af1e34931a
alvin <alvin.odins@gmail.com>
2017-06-19 13:12:50 -0700

    add advertising and socialmedia component dirs

---
+++ app/services/consulting/consulting.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'consulting.component.html'
+})
+
+export class ConsultingComponent implements OnInit {
+
+  ngOnInit(){}
+}
---
+++ app/services/socialmedia/socialmedia.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'socialmedia.component.html'
+})
+
+export class SocialmediaComponent implements OnInit {
+
+  ngOnInit(){}
+}
